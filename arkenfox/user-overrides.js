4c1b97839d26e2be636b0ec8d397aafff6ca2fc9
ma4cek <ma4cek@posteo.de>
2023-05-28 17:19:44 +0200

    Initial dotfile config

---
+++ arkenfox/user-overrides.js
@@ -0,0 +1,39 @@
+/* override recipe: enable DRM and let me watch videos ***/
+user_pref("media.eme.enabled", true); // 2022
+/* 0804: disable live search suggestions
+ * [NOTE] Both must be true for the location bar to work
+ * [SETUP-CHROME] Override these if you trust and use a privacy respecting search engine
+ * [SETTING] Search>Provide search suggestions | Show search suggestions in address bar results ***/
+user_pref("browser.search.suggest.enabled", true);
+user_pref("browser.urlbar.suggest.searches", true);
+/* 0801: disable location bar using search
+ * Don't leak URL typos to a search engine, give an error message instead
+ * Examples: "secretplace,com", "secretplace/com", "secretplace com", "secret place.com"
+ * [NOTE] This does not affect explicit user action such as using search buttons in the
+ * dropdown, or using keyword search shortcuts you configure in options (e.g. "d" for DuckDuckGo)
+ * [SETUP-CHROME] Override this if you trust and use a privacy respecting search engine ***/
+user_pref("keyword.enabled", true);
+/* 1601: control when to send a cross-origin referer
+ * 0=always (default), 1=only if base domains match, 2=only if hosts match
+ * [SETUP-WEB] Breakage: older modems/routers and some sites e.g banks, vimeo, icloud, instagram
+ * If "2" is too strict, then override to "0" and use Smart Referer extension (Strict mode + add exceptions) ***/
+user_pref("network.http.referer.XOriginPolicy", 0);
+/* 4501: enable privacy.resistFingerprinting [FF41+]
+ * [SETUP-WEB] RFP can cause some website breakage: mainly canvas, use a site exception via the urlbar
+ * RFP also has a few side effects: mainly timezone is UTC0, and websites will prefer light theme
+ * [1] https://bugzilla.mozilla.org/418986 ***/
+user_pref("privacy.resistFingerprinting", false);
+/* 4504: enable RFP letterboxing [FF67+]
+ * Dynamically resizes the inner window by applying margins in stepped ranges [2]
+ * If you use the dimension pref, then it will only apply those resolutions.
+ * The format is "width1xheight1, width2xheight2, ..." (e.g. "800x600, 1000x1000")
+ * [SETUP-WEB] This is independent of RFP (4501). If you're not using RFP, or you are but
+ * dislike the margins, then flip this pref, keeping in mind that it is effectively fingerprintable
+ * [WARNING] DO NOT USE: the dimension pref is only meant for testing
+ * [1] https://bugzilla.mozilla.org/1407366
+ * [2] https://hg.mozilla.org/mozilla-central/rev/6d2d7856e468#l2.32 ***/
+user_pref("privacy.resistFingerprinting.letterboxing", false); // [HIDDEN PREF]
+/* 4520: disable WebGL (Web Graphics Library)
+ * [SETUP-WEB] If you need it then override it. RFP still randomizes canvas for naive scripts ***/
+user_pref("webgl.disabled", false);
+
---
+++ fish/config.fish
@@ -0,0 +1,27 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    starship init fish | source
+end
+
+# Force Mozilla to use Wayland
+set -x MOZ_ENABLE_WAYLAND 1
+# Force Steam to use Wayland
+set -x SDL_VIDEODRIVER 'wayland'
+# set -x QT_STYLE_OVERRIDE 'kvantum'
+
+# recomended enviroment variables for sway based desktop
+# openSUSEway installs/imports this to/from:
+# /usr/lib/environment.d/50-openSUSE.conf
+# /etc/profile.d/openSUSEway.sh
+set -x QT_QPA_PLATFORM 'wayland-egl'
+set -x CLUTTER_BACKEND 'wayland'
+set -x ECORE_EVAS_ENGINE 'wayland-egl'
+set -x ELM_ENGINE 'wayland_egl'
+set -x _JAVA_AWT_WM_NONREPARENTING 1
+set -x NO_AT_BRIDGE 1
+
+# QT theme for openSUSEway
+# needs qt5ct and adwaita-qt5 packages
+# default config is /etc/xdg/qt5ct/qt5ct.conf
+set -x QT_QPA_PLATFORMTHEME 'qt5ct'
+
